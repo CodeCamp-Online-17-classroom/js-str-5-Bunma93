5c45a07822b31f3210975012293b827bd3734ba4
Bunma93 <bunma93@gmail.com>
2024-07-08 16:12:34 +0700

    Js-str5

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // write code here
+function truncate(str, length) {
+    if (str.length > length){
+        alert(str.slice(0,length)+ "...");
+    } else {
+        alert(str);
+    }
+}
+
+truncate("What I'd like to tall on this topic is:", 20); // What I'd like to te...
+truncate("Hi everyone!", 20); // Hi everyone!
